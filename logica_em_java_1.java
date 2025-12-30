33e9042a5d648835d36bc07bbe9d73f0529a9cbc
joao <jovmamikl@gmail.com>
2025-12-30 15:59:59 -0300

    first commit

---
+++ logica_em_java_1.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+
+public class logica_em_java_1 {
+    public static void main(String[] args){
+    int idadeAno, idadeMes, idadeDia;
+    Scanner leitura = new Scanner(System.in);
+
+    System.out.println("Irei mostrar sua idade em em dias, preciso apenas que me falei a idade em anos, meses e dias e irei converter para dias\n");
+    
+    System.out.println("Digite sua idade em anos: \n");
+    idadeAno = leitura.nextInt();
+
+    System.out.println("Digite sua idade em meses: \n");
+    idadeMes = leitura.nextInt();
+
+    System.out.println("Digite sua idade em dias: \n");
+    idadeDia = leitura.nextInt();
+
+    int opAno = (idadeAno*365);
+    int opMes = (idadeMes*30);
+
+    int totalDias = opAno + opMes + idadeDia;
+    
+    System.out.printf("Anos em dia: %d | Meses em dia : %d | Dias: %d | Total de dias: %d", opAno,opMes,idadeDia, totalDias);
+
+    
+
+
+
+    }
+}
---
+++ logica_em_java_2.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class logica_em_java_2 {
+    
+    public static void main(String[] args) {
+    double a = 8, b = 9, c = 7;
+    double d = 4, e = 5, f = 6;
+
+
+    System.out.println("\n");
+    
+  
+
+    double opMediaUm = (a + b+ c)/(3);
+
+    double opMediaDois = (d + e+ f)/(3);
+
+    double somaDuasMedias = opMediaUm + opMediaDois;
+
+    double mediaDasMedias = (opMediaUm+opMediaDois )/(2);
+
+    System.out.print(opMediaUm);
+
+
+
+    }
+}
